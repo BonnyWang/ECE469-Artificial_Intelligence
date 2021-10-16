99dba800c75e11766ffb4cfde1dde52d53fcea2a
BonnyWang <bonny.y.gardy@gmail.com>
2021-10-15 23:14:28 -0400

    Added load file function

--- PS1-Othello/main.cpp
+++ PS1-Othello/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include<string>
+#include <fstream>
 using namespace std;
 
+// Player 1 is X, player 2 is O
 static char board[8][8];
+static int turn;
+static int timeLimit;
 
 void drawBoard(){
     cout<< "      1   2   3   4   5   6   7   8 " << endl;
@@ -21,12 +26,87 @@ void initializeBoard(){
             board[i][j] = ' ';
         }
     }
+
+    board[3][3] = 'O';
+    board[3][4] = 'X';
+    board[4][3] = 'X';
+    board[4][4] = 'O';
+}
+
+// Load board from a given file and play based on it
+void loadBoard(string fileName){
+    ifstream file;
+    string line;
+    int lineNumber = 1;
+    int row = 0;
+    int column = 0;
+
+    file.open(fileName);
+    
+    if(file.is_open()){
+        while(getline(file, line)){
+            if(row < 8){
+                for(int i = 0; i < sizeof(line); i++){
+                    if(isdigit(line[i])){
+                        switch (line[i]){
+                            case '1':
+                                board[row][column] = 'X';
+                                break;
+                            case '2':
+                                board[row][column] = 'O';
+                                break;
+                            default:
+                                board[row][column] = ' ';
+                                break;
+                        }
+                        column++;
+                    }   
+                }
+                row++;
+                column = 0;
+            }
+
+            if(lineNumber == 9){
+                turn = line[0];
+            }
+
+            if(lineNumber == 10){
+                timeLimit = line[0];
+            }
+
+            lineNumber++;
+        }
+
+        file.close();
+    }else{
+        cout << "Unable to open the file!" << endl;
+        initializeBoard();
+    } 
+
 }
 
 int main(){
+    
+    char loadFromFile;
+    string fileName;
+    
     cout << "Welcome to the Othello paradise!" << endl;
-    initializeBoard();
+    cout << "Do you want to load the Board from an file?(Y/N)"<< endl;
+    cin >> loadFromFile;
+    if(loadFromFile == 'Y'){
+        cout <<"Enter the file name:" << endl;
+        cin >> fileName;
+        loadBoard(fileName);
+    }else if (loadFromFile == 'N'){
+        initializeBoard();
+    }else{
+        cout << "Invalid input, load from the start." << endl;
+        initializeBoard();
+    }
+
+
     drawBoard();
+  
     return 0;
 }
 
