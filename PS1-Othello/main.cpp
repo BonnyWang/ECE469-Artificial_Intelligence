7c9ef9207e75b741abbea0f19a7aa8883fe587fb
BonnyWang <bonny.y.gardy@gmail.com>
2021-10-24 21:23:22 -0400

    ask for roles

--- PS1-Othello/main.cpp
+++ PS1-Othello/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include<string>
 #include <fstream>
+#define COMPUTER 0
+#define HUMAN 1
 using namespace std;
+
 
 // Player 1 is X, player 2 is O
 static char board[8][8];
 static int turn;
 static int timeLimit;
+static int playerX;
+static int playerO;
+static bool gameEnd;
 
 void drawBoard(){
     cout<< "      1   2   3   4   5   6   7   8 " << endl;
@@ -87,10 +93,11 @@ void loadBoard(string fileName){
 
 }
 
-int main(){
-    
+void initialAttempt(){
     char loadFromFile;
     string fileName;
+    string player1Role;
+    string player2Role;
     
     cout << "Welcome to the Othello paradise!" << endl;
     cout << "Do you want to load the Board from an file?(Y/N)"<< endl;
@@ -106,9 +113,33 @@ int main(){
         initializeBoard();
     }
 
+    cout << "Player for X? 1.you 2.Computer (1/2)" << endl;
+    cin >> player1Role;
+    if(player1Role == "1"){
+        playerX = HUMAN;
+    }else{
+        // If user entered invalid then playerX is the computer by default
+        playerX = COMPUTER;
+    }
+
+    cout << "Player for O? 1.you 2.Computer (1/2)" << endl;
+    cin >> player2Role;
+    if(player2Role == "1"){
+        playerX = HUMAN;
+    }else{
+        // If user entered invalid then playerO is the computer by default
+        playerX = COMPUTER;
+    }
 
     drawBoard();
-  
+}
+
+void gameCoreLoop(){
+    
+}
+
+int main(){
+    initialAttempt();
     return 0;
 }
 
