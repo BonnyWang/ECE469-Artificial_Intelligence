b3232e2389f5c1d38ce3cf373fa41bb5cd1a4c6a
BonnyWang <bonny.y.gardy@gmail.com>
2021-11-27 22:07:46 -0500

    start

--- PS2-NN/trainNN.py
+++ PS2-NN/trainNN.py
@@ -1,5 +1,28 @@
-def askInput():
-    input("Enter the name of:");
+# Initalize variables
+nInputNodes = 0;
+nOutNodes = 0;
+nHiddenNodes = 0;
+
+class node:
+    edgeInWeights = [];
+    edgeOutWeigths = [];
+
+
+def preProcess():
+    initialFile = input("Enter the name of the intial neural network:");
+    trainFile = input("Enter the name of the training set:");
+    outputFile = input("Enter the name of the output file:");
+
+    fd = open(initialFile);
+    lines = fd.readlines();
+
+    nInputNodes = lines[0].split(" ")[0];
+    nHiddenNodes = lines[0].split()[1];
+    nOutNodes = lines[0].split()[2];
+
+    print("hi")
+    # for line in fd.readlines():
+    #     # if line
 
 if __name__ == "__main__":
-    askInput();
+    preProcess();
